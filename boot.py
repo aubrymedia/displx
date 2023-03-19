20b31fa8f4d9edaba2572c29fc9a2be500c587b7
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-20 00:05:51 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,7 +1,7 @@
 import os
 
 # Téléchargez le script player.py depuis le dépôt GitHub
-os.system('wget https://raw.githubusercontent.com/aubrymedia/displx/main/config.py')
+os.system('wget -N https://raw.githubusercontent.com/aubrymedia/displx/main/config.py')
 
 # Exécutez le script player.py
 os.system('python3 config.py')
