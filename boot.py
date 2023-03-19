ed18bdb3401514bcf771444a1e31775c580405be
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-19 23:58:01 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,12 +1,7 @@
 import os
 
-# Installez les bibliothèques nécessaires
-os.system('sudo apt-get update')
-os.system('sudo apt-get install -y dropbox vlc python3-pip')
-os.system('pip3 install requests beautifulsoup4')
-
 # Téléchargez le script player.py depuis le dépôt GitHub
-os.system('wget https://raw.githubusercontent.com/aubrymedia/displx/main/player.py')
+os.system('wget https://raw.githubusercontent.com/aubrymedia/displx/main/config.py')
 
 # Exécutez le script player.py
-os.system('python3 player.py')
+os.system('python3 config.py')
