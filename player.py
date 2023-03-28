27c3bcbbdc94343257a1c9f224347d0d78e63dee
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-29 00:16:58 +0200

    Update player.py

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import dropbox
+import sqlite3
 import av
 
 # URL de la page contenant le token d'accès
@@ -26,10 +27,16 @@ if not os.path.exists(local_folder):
 dbx = dropbox.Dropbox(access_token)
 
 file_lock = threading.Lock()  # Ajouter un verrou pour les fichiers
-downloaded_files = []  # Liste pour stocker les noms de fichiers téléchargés
 
-def download_videos():
-    global file_lock, downloaded_files
+def list_files(path):
+    files = []
+    for entry in os.scandir(path):
+        if entry.is_file() and entry.name.endswith(".mp4"):
+            files.append(entry.name)
+    return files
+
+def download_videos(cursor):
+    global file_lock
 
     result = dbx.files_list_folder(dbx_folder)
 
@@ -37,55 +44,63 @@ def download_videos():
         if isinstance(entry, dropbox.files.FileMetadata) and entry.name.endswith(".mp4"):
             local_file_path = os.path.join(local_folder, entry.name)
 
-            if entry.name not in downloaded_files:
+            if cursor.execute('SELECT * FROM files WHERE name=?', (entry.name,)).fetchone() is None:
                 with open(local_file_path, "wb") as f:
                     metadata, res = dbx.files_download(entry.path_lower)
                     f.write(res.content)
 
-                downloaded_files.append(entry.name)
+                cursor.execute('INSERT INTO files VALUES (?)', (entry.name,))
+                cursor.connection.commit()
 
     with file_lock:  # Acquérir le verrou avant de supprimer les fichiers
         local_files = list_files(local_folder)
-        for file in local_files:
-            if file not in downloaded_files:
-                os.remove(os.path.join(local_folder, file))
-
-def list_files(path):
-    files = []
-    for entry in os.scandir(path):
-        if entry.is_file() and entry.name.endswith(".mp4"):
-            files.append(os.path.join(path, entry.name))
-    return files
+        for file in cursor.execute('SELECT * FROM files'):
+            if file[0] not in local_files:
+                os.remove(os.path.join(local_folder, file[0]))
+                cursor.execute('DELETE FROM files WHERE name=?', (file[0],))
+                cursor.connection.commit()
 
 def download_videos_loop():
+    conn = sqlite3.connect('file_cache.db')
+    cursor = conn.cursor()
+
     while True:
-        download_videos()
+        download_videos(cursor)
         time.sleep(60)  # Télécharge les nouvelles vidéos toutes les 60 secondes
+
+    conn.close()
 
 def play_videos():
     global file_lock
 
     while True:
-        video_files = list_files(local_folder)
+        container = av.open(local_folder)
+        video_files = [f.name for f in container.streams.video]
 
         if not video_files:
+            container.close()
             continue
 
         instance = av.Player()
 
         for video_file in video_files:
             with file_lock:  # Acquérir le verrou avant d'ouvrir le fichier avec VLC
-                stream = av.open(video_file).streams.video[0]
+                stream = container.streams.video[video_file]
                 instance.play(stream)
 
             while instance.playing:
                 time.sleep(1)
 
                 with file_lock:
-                    current_video_files = list_files(local_folder)
+                    current_container = av.open(local_folder)
+                    current_video_files = [f.name for f in current_container.streams.video]
 
                     if video_files != current_video_files:
+                        container.close()
+                        container = current_container
                         break
+
+        container.close()
 
 if __name__ == "__main__":
     download_thread = threading.Thread(target=download_videos_loop)
