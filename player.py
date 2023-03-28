f5abf1a2e081550414e1105904a76ff549f96bab
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-28 23:24:53 +0200

    Update player.py

--- player.py
+++ player.py
@@ -1,8 +1,4 @@
 import os
-import subprocess
-import dropbox
-import vlc
-import time
 import threading
 import time
 import requests
@@ -14,28 +10,28 @@ import av
 # URL de la page contenant le token d'accès
 access_token_url = "https://aubrymedia.com/keyapi/"
 
-# Dossier Dropbox contenant les vidéos à télécharger
-dbx_folder = "/"
+# Récupération du contenu de la page
+response = requests.get(access_token_url)
+soup = BeautifulSoup(response.content, "html.parser")
 
-# Dossier local où stocker les vidéos
-local_folder = "/home/pi/videos"
+# Extraction du token d'accès depuis la balise H1
+access_token = soup.find("h1").text
 
-# Verrou pour empêcher la lecture de vidéos en cours de téléchargement ou de suppression
-file_lock = threading.Lock()
+dbx_folder = "/Displex"
+local_folder = "/home/pi/contents"
 
-# Obtenez le jeton d'accès à l'API Dropbox en visitant la page access_token_url
-access_token = requests.get(access_token_url).text.strip()
+# Crée le dossier local si nécessaire
+if not os.path.exists(local_folder):
+    os.makedirs(local_folder)
 
-# Instancier le client Dropbox
 dbx = dropbox.Dropbox(access_token)
 
+file_lock = threading.Lock()  # Ajouter un verrou pour les fichiers
+
 def list_files(path):
-    """
-    Retourne une liste de noms de fichiers dans le dossier spécifié.
-    """
     files = []
     for entry in os.scandir(path):
-        if entry.is_file():
+        if entry.is_file() and entry.name.endswith(".mp4"):
             files.append(entry.name)
     return files
 
@@ -43,26 +39,20 @@ def download_videos(cursor):
     global file_lock
 
     result = dbx.files_list_folder(dbx_folder)
-    local_files = list_files(local_folder)
 
     for entry in result.entries:
         if isinstance(entry, dropbox.files.FileMetadata) and entry.name.endswith(".mp4"):
             local_file_path = os.path.join(local_folder, entry.name)
 
-            if entry.name not in local_files:
             if cursor.execute('SELECT * FROM files WHERE name=?', (entry.name,)).fetchone() is None:
                 with open(local_file_path, "wb") as f:
                     metadata, res = dbx.files_download(entry.path_lower)
                     f.write(res.content)
 
-            if entry.name in local_files:
-                local_files.remove(entry.name)
                 cursor.execute('INSERT INTO files VALUES (?)', (entry.name,))
                 cursor.connection.commit()
 
     with file_lock:  # Acquérir le verrou avant de supprimer les fichiers
-        for file in local_files:
-            os.remove(os.path.join(local_folder, file))
         local_files = list_files(local_folder)
         for file in cursor.execute('SELECT * FROM files'):
             if file[0] not in local_files:
@@ -75,7 +65,6 @@ def download_videos_loop():
     cursor = conn.cursor()
 
     while True:
-        download_videos()
         download_videos(cursor)
         time.sleep(60)  # Télécharge les nouvelles vidéos toutes les 60 secondes
 
@@ -84,45 +73,23 @@ def download_videos_loop():
 def play_videos():
     global file_lock
 
-    video_files = list_files(local_folder)
     while True:
         container = av.open(local_folder)
         video_files = [f.name for f in container.streams.video]
 
-    if not video_files:
-        return
         if not video_files:
             container.close()
             continue
 
-    instance = vlc.Instance("--no-xlib")
-    player = instance.media_player_new()
         instance = av.Player()
 
-    while True:
         for video_file in video_files:
             with file_lock:  # Acquérir le verrou avant d'ouvrir le fichier avec VLC
-                media = instance.media_new(os.path.join(local_folder, video_file))
-                player.set_media(media)
-            player.audio_toggle_mute()  # Désactiver l'audio
-            player.play()
-            player.set_fullscreen(True)
-            time.sleep(1)
-
-            while player.get_state() != vlc.State.Ended:
                 stream = container.streams.video[video_file]
                 instance.play(stream)
 
             while instance.playing:
                 time.sleep(1)
-                current_video_files = list_files(local_folder)
-                if video_files != current_video_files:
-                    video_files = current_video_files
-                    player.stop()
-                    break
-
-            if player.get_state() == vlc.State.Ended:
-                video_files = list_files(local_folder)
 
                 with file_lock:
                     current_container = av.open(local_folder)
@@ -137,3 +104,6 @@ def play_videos():
 
 if __name__ == "__main__":
     download_thread = threading.Thread(target=download_videos_loop)
+    download_thread.start()
+
+    play_videos()
