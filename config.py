7f9646a945a4780a41880b05a3f75fce32a131c7
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-29 00:25:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,17 @@
 import os
-import subprocess
-import urllib.request
 
-# def connect_to_wifi(ssid, password):
-#     # Modifiez le fichier wpa_supplicant.conf pour ajouter les informations du réseau WiFi
-#     with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as f:
-#         f.write(f'network={{\n    ssid="{ssid}"\n    psk="{password}"\n}}\n')
+# Chemin du dossier displex
+displex_folder = "/home/pi/displex"
 
-#     # Redémarrez les services wpa_supplicant et dhcpcd pour appliquer les modifications
-#     subprocess.run(['sudo', 'systemctl', 'restart', 'dhcpcd.service', 'wpa_supplicant.service'])
+# Créer le dossier s'il n'existe pas
+if not os.path.exists(displex_folder):
+    os.makedirs(displex_folder)
 
-def download_and_run_script(url):
-    try:
-        # Téléchargez le script à partir de l'URL
-        urllib.request.urlretrieve(url, 'player.py')
+# Chemin du fichier player.py
+player_path = os.path.join(displex_folder, "player.py")
 
-        # Exécutez le script
-        subprocess.run(['python3', 'player.py'])
-    except Exception as e:
-        print(f'Une erreur s\'est produite lors du téléchargement ou de l\'exécution du script: {e}')
+# Télécharger le fichier player.py depuis le dépôt GitHub et remplacer la version existante s'il y en a une
+os.system(f"wget -O {player_path} https://raw.githubusercontent.com/aubrymedia/displx/main/player.py")
 
-
-# Connectez-vous au réseau WiFi
-# connect_to_wifi('nom_du_reseau_wifi', 'mot_de_passe_wifi')
-
-# Téléchargez et exécutez le script player.py depuis le dépôt GitHub
-download_and_run_script('https://raw.githubusercontent.com/aubrymedia/displx/main/player.py')
+# Exécuter le script player.py
+os.system(f"python3 {player_path}")
