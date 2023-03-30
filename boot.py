11238bbdff8d817b86e9f8ee7166fc08ee9fee19
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-30 13:49:13 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -10,7 +10,11 @@ if not os.path.exists(displex_folder):
 # Chemin du fichier config.py
 config_path = os.path.join(displex_folder, "config.py")
 
-# Télécharger le fichier config.py depuis le dépôt GitHub et remplacer la version existante s'il y en a une
+# Supprimer le fichier config.py s'il existe
+if os.path.exists(config_path):
+    os.remove(config_path)
+
+# Télécharger le fichier config.py depuis le dépôt GitHub
 os.system(f"wget -O {config_path} https://raw.githubusercontent.com/aubrymedia/displx/main/config.py")
 
 # Exécuter le script config.py
