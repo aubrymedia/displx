e8971939cdf24fd515b0443ff5458d5ee607f92b
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-30 13:41:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,17 @@ displex_folder = "/home/pi/displex"
 if not os.path.exists(displex_folder):
     os.makedirs(displex_folder)
 
-# Chemin du fichier player.py
+# Chemin des fichiers config.py et player.py
+config_path = os.path.join(displex_folder, "config.py")
 player_path = os.path.join(displex_folder, "player.py")
 
-# Télécharger le fichier player.py depuis le dépôt GitHub et remplacer la version existante s'il y en a une
+# Supprimer les fichiers config.py et player.py s'ils existent
+if os.path.exists(config_path):
+    os.remove(config_path)
+if os.path.exists(player_path):
+    os.remove(player_path)
+
+# Télécharger le fichier player.py depuis le dépôt GitHub
 os.system(f"wget -O {player_path} https://raw.githubusercontent.com/aubrymedia/displx/main/player.py")
 
 # Exécuter le script player.py
