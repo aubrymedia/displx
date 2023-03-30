fb9e799caffcc21c4fb79fe4f4c838f267fa4dc7
aubrymedia <82332786+aubrymedia@users.noreply.github.com>
2023-03-30 13:37:32 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,7 +1,17 @@
 import os
 
-# Téléchargez le script player.py depuis le dépôt GitHub
-os.system('wget -N https://raw.githubusercontent.com/aubrymedia/displx/main/player.py')
+# Chemin du dossier displex
+displex_folder = "/home/pi/displex"
 
-# Exécutez le script player.py
-os.system('python3 player.py')
+# Créer le dossier s'il n'existe pas
+if not os.path.exists(displex_folder):
+    os.makedirs(displex_folder)
+
+# Chemin du fichier config.py
+config_path = os.path.join(displex_folder, "config.py")
+
+# Télécharger le fichier config.py depuis le dépôt GitHub et remplacer la version existante s'il y en a une
+os.system(f"wget -O {config_path} https://raw.githubusercontent.com/aubrymedia/displx/main/config.py")
+
+# Exécuter le script config.py
+os.system(f"python3 {config_path}")
